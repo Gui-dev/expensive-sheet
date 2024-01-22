cdac9e205540550544c51ed00e043bfcbe1c6750
Adriano Silva <adrianon10@gmail.com>
2024-01-22 04:57:48 -0300

    test: it should be able login for the user to log in with correct credentials

--- backend/src/modules/session/__tests__/session-supertest.spec.ts
+++ backend/src/modules/session/__tests__/session-supertest.spec.ts
@@ -1,13 +1,35 @@
 import supertest from 'supertest'
+import bcrypt from 'bcrypt'
 
 import { app } from './../../../server'
+import { prisma } from '../../../services/prisma'
 
 describe('Session /login', () => {
+  beforeAll(async () => {
+    const name = 'Bruce Wayne'
+    const email = 'bruce@email.com'
+    const password = '123456'
+
+    const hashed_password = await bcrypt.hash(password, 10)
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashed_password,
+      },
+    })
+  })
+
+  afterAll(async () => {
+    await prisma.user.deleteMany()
+  })
+
   it('should not be able login for the user to log in with the wrong email address', async () => {
     const email = 'fake_email@email.com'
     const password = '123456'
 
-    const result = await supertest(app.callback())
+    const result = await supertest(app.listen())
       .get('/login')
       .auth(email, password)
 
@@ -18,10 +40,24 @@ describe('Session /login', () => {
     const email = 'bruce@email.com'
     const password = 'fake_password'
 
-    const result = await supertest(app.callback())
+    const result = await supertest(app.listen())
       .get('/login')
       .auth(email, password)
 
     expect(result.statusCode).toBe(401)
+  })
+
+  it('should be able login for the user to log in with correct credentials', async () => {
+    const email = 'bruce@email.com'
+    const password = '123456'
+
+    const result = await supertest(app.listen())
+      .get('/login')
+      .auth(email, password)
+
+    expect(result.statusCode).toBe(200)
+    expect(result.body).toBeTruthy()
+    expect(result.body.user.id).toBeTruthy()
+    expect(result.body.token).toBeTruthy()
   })
 })

db7197a6ee574d96301146651bbcc01fb2970ad1
Adriano Silva <adrianon10@gmail.com>
2024-02-02 04:35:44 -0300

    feat: add helper function handleRegister

--- frontend/src/app/components/form-signup.tsx
+++ frontend/src/app/components/form-signup.tsx
@@ -1,23 +1,52 @@
 'use client'
 
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { Loader, UserCheck } from 'lucide-react'
+import { AxiosError } from 'axios'
 
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
-import { UserCheck } from 'lucide-react'
+import {
+  SignupValidationData,
+  signupValidation,
+} from '@/validations/signup-validation'
+import { api } from '@/services/api'
 
 export const FormSignup = () => {
-  const { handleSubmit } = useForm()
+  const [loading, setLoading] = useState(false)
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    setError,
+  } = useForm<SignupValidationData>({
+    resolver: zodResolver(signupValidation),
+  })
 
-  const handleRegister = () => {
-    console.log('AQIII')
+  const handleRegister = async (data: SignupValidationData) => {
+    try {
+      setLoading(true)
+      const response = await api.post('/users', { ...data })
+      setLoading(false)
+      console.log('DATA: ', response.data)
+    } catch (error) {
+      const err = error as AxiosError
+      if (err.response?.status === 409) {
+        setError('email', {
+          message: 'E-mail já foi cadastrado, utilize outro endereço de email',
+        })
+      }
+      setLoading(false)
+    }
   }
 
   return (
     <form
       onSubmit={handleSubmit(handleRegister)}
-      className="flex flex-col gap-4"
+      className="flex w-full max-w-[75%] flex-col gap-4"
     >
       <div className="flex flex-col gap-4">
         <Label htmlFor="name">Nome</Label>
@@ -26,7 +55,11 @@ export const FormSignup = () => {
           placeholder="Digite aqui o seu Nome"
           id="name"
           className="border border-xs-green"
+          {...register('name')}
         />
+        {errors && errors.name && (
+          <span className="text-xs text-xs-red">{errors.name.message}</span>
+        )}
       </div>
       <div className="flex flex-col gap-4">
         <Label htmlFor="email">E-mail</Label>
@@ -35,7 +68,11 @@ export const FormSignup = () => {
           placeholder="Digite aqui o seu E-mail"
           id="email"
           className="border border-xs-green"
+          {...register('email')}
         />
+        {errors && errors.email && (
+          <span className="text-xs text-xs-red">{errors.email.message}</span>
+        )}
       </div>
 
       <div className="flex flex-col gap-4">
@@ -45,12 +82,21 @@ export const FormSignup = () => {
           placeholder="Digite aqui a sua Senha"
           id="password"
           className="border border-xs-green"
+          {...register('password')}
         />
+        {errors && errors.password && (
+          <span className="text-xs text-xs-red">{errors.password.message}</span>
+        )}
       </div>
 
-      <Button className="mt-8">
-        <UserCheck className="mr-2 h-4 w-4" />
-        Registrar
+      <Button type="submit" className="mt-8" disabled={loading}>
+        {loading && <Loader className="h-4 w-4 animate-spin text-white" />}
+        {!loading && (
+          <>
+            <UserCheck className="mr-2 h-4 w-4" />
+            Registrar
+          </>
+        )}
       </Button>
     </form>
   )
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
-import { Signup } from '@/app/components/signup'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  const user = false
+
+  if (!user) {
+    return redirect('/signin')
+  }
+
   return (
     <main className="flex min-h-screen px-8">
-      <Signup />
+      <h1>HOME</h1>
     </main>
   )
 }

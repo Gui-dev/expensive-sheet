6e36cfb0bc0172207bf64e493632c5ea242e31e3
Adriano Silva <adrianon10@gmail.com>
2024-02-06 04:41:16 -0300

    feat: add auth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import { useContext } from 'react'
+
+import { AuthContext } from '@/contexts/auth'
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+
+  return context
+}

310bffeddbe045adc9061b4fd8c5c25a72c8cca1
Adriano Silva <adrianon10@gmail.com>
2024-02-22 05:06:11 -0300

    feat: add signin validation

---
+++ mobile/src/validations/signin-validation.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+export const signinValidation = z.object({
+  email: z
+    .string({
+      required_error: 'O e-mail é obrigatório',
+    })
+    .min(1, {
+      message: 'O e-mail é obrigatório',
+    })
+    .email({
+      message: 'Digite um e-mail válido',
+    }),
+  password: z
+    .string({
+      required_error: 'A senha é obrigatório',
+    })
+    .min(6, {
+      message: 'Sua senha deve ter no mínimo 6 caracteres',
+    }),
+})
+
+export type SigninValidationData = z.infer<typeof signinValidation>

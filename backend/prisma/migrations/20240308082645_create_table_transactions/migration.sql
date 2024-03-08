2d40cb63908a1ca652f0ddf138ad9d2ab9c60a60
Adriano Silva <adrianon10@gmail.com>
2024-03-08 05:27:30 -0300

    feat: add migration create table transactions

---
+++ backend/prisma/migrations/20240308082645_create_table_transactions/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Recurrence" AS ENUM ('daily', 'montlhy', 'yearly');
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "recurrence" "Recurrence",
+    "installments" INTEGER,
+    "is_subscription" BOOLEAN,
+    "due_date" TIMESTAMP(3),
+    "resolved" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

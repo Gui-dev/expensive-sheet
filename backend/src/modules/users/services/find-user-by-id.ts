ff38bf7de0db3996d1588738e9e031aeddde89ec
Adriano Silva <adrianon10@gmail.com>
2024-03-09 05:07:51 -0300

    feat: add helper function findUserById

---
+++ backend/src/modules/users/services/find-user-by-id.ts
@@ -0,0 +1,24 @@
+import { type User } from '@prisma/client'
+import { AppError } from '../../../shared/error/app-error'
+import { userView } from '../../../shared/views/user'
+import { findUserByIdRepository } from '../repositories/find-user-by-id'
+
+interface IFindUserById {
+  user_id: string
+}
+
+export const findUserById = async ({
+  user_id,
+}: IFindUserById): Promise<
+  Omit<User, 'password' | 'updated_at' | 'deleted_at'>
+> => {
+  const user_exists = await findUserByIdRepository({ user_id })
+
+  if (!user_exists) {
+    throw new AppError('User not found', 404)
+  }
+
+  const user_view = userView(user_exists)
+
+  return user_view
+}

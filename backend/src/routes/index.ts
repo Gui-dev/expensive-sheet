973cf14e2fc11f976012db332c93ea35800eb0ee
Adriano Silva <adrianon10@gmail.com>
2024-03-13 04:36:48 -0300

    feat: add check authorization middleware

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,19 @@
 import Router from '@koa/router'
 
 import { userCreate, userList, userRemove, userUpdate } from '../modules/users'
+import { createTransaction } from './../modules/transactions/transaction'
 import { login } from '../modules/session'
+import { check_authorization } from '../shared/middlewares/check-authorization'
 
 const router = new Router()
 
-router.get('/users', userList)
 router.post('/users', userCreate)
-router.put('/users/:user_id', userUpdate)
-router.delete('/users/:user_id', userRemove)
-
 router.get('/login', login)
+
+router.get('/users', check_authorization, userList)
+router.put('/users/:user_id', check_authorization, userUpdate)
+router.delete('/users/:user_id', check_authorization, userRemove)
+
+router.post('/transactions', check_authorization, createTransaction)
 
 export { router }
---
+++ backend/src/shared/middlewares/check-authorization.ts
@@ -0,0 +1,29 @@
+import { type Context, type Next } from 'koa'
+import jwt from 'jsonwebtoken'
+
+import { AppError } from '../error/app-error'
+
+export const check_authorization = async (
+  ctx: Context,
+  next: Next,
+): Promise<void> => {
+  try {
+    const authorization = ctx.request.headers.authorization
+
+    if (!authorization) {
+      throw new AppError('Token not found', 401)
+    }
+    const [type, token] = authorization.split(' ')
+
+    if (type !== 'Bearer' || !token) {
+      throw new AppError('Token format invalid', 401)
+    }
+
+    const decoded = jwt.decode(token)
+    ctx.user_id = decoded?.sub
+    await next()
+  } catch (error) {
+    console.log('TOKEN_ERROR: ', error)
+    throw new AppError('Token not found', 401)
+  }
+}

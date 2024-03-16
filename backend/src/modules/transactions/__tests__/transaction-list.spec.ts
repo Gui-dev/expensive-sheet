f92c8742ccdbab6257a44011965f11ecad9bf4b7
Adriano Silva <adrianon10@gmail.com>
2024-03-16 05:09:04 -0300

    test: it should be able to list all transactions to logged in user

--- backend/src/modules/transactions/__tests__/transaction-list.spec.ts
+++ backend/src/modules/transactions/__tests__/transaction-list.spec.ts
@@ -1,10 +1,10 @@
 import { type User } from '@prisma/client'
 
-// import { createTransactionUseCase } from '../use-cases/create-transaction-use-case'
+import { createTransactionUseCase } from '../use-cases/create-transaction-use-case'
 import { listTransactionUsecase } from '../use-cases/list-transaction-use-case'
 import { AppError } from '../../../shared/error/app-error'
 import { createUserRepository } from '../../users/repositories/create-user'
-// import { sessionUseCase } from '../../session/use-cases/session-use-case'
+import { sessionUseCase } from '../../session/use-cases/session-use-case'
 
 let user: User
 
@@ -18,11 +18,33 @@ describe('Transactions', () => {
     })
   })
 
-  it('should not be able to list transaction without auth', async () => {
+  it('should not be able to list transactions without auth', async () => {
     const user_id = 'fake_user_id'
 
     await expect(listTransactionUsecase({ user_id })).rejects.toBeInstanceOf(
       AppError,
     )
+  })
+  it('should be able to list all transactions to logged in user', async () => {
+    const description = 'fake_description'
+    const value = 10
+    const response = await sessionUseCase({
+      email: user.email,
+      password: '123456',
+    })
+    await createTransactionUseCase({
+      user_id: response.user.id,
+      description,
+      value,
+    })
+    await createTransactionUseCase({
+      user_id: response.user.id,
+      description,
+      value,
+    })
+    const list_transactions = await listTransactionUsecase({
+      user_id: response.user.id,
+    })
+    expect(list_transactions.length).toBe(2)
   })
 })
